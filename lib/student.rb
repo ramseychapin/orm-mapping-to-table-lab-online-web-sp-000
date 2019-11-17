8301f3b73275347311a58baf99594b1fde0f27f3
Ramsey Chapin <ramsey@ramseychapin.com>
2019-11-17 21:53:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,10 @@ class Student
 
   attr_accessor :name, :grade
 
-  
+  def initialize(name, grade)
+
+
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
