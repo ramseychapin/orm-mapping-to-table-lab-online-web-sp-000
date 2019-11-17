a68fc91a1dca72bd8efe49f3b647db2fc09e55b0
Ramsey Chapin <ramsey@ramseychapin.com>
2019-11-17 22:04:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,8 +29,7 @@ class Student
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name,
-grade)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
