ca2762657aaf6ae4cce0801203aefb695bebfecd
Ramsey Chapin <ramsey@ramseychapin.com>
2019-11-17 21:59:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,11 +22,7 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-      )
+    DROP TABLE students;
     SQL
     DB[:conn].execute(sql)
   end
